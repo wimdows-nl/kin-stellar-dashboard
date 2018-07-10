d23fe37925bc0d4c28067a6d938ea527e3bd7085
Chancey <hawbakerchancey@gmail.com>
2018-07-10 10:02:53 -0700

    Add project files.

---
+++ kin-stellar-dashboard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kin_stellar_dashboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
